5dd7bf974e621d529f9270506bdbd05240b6891d
joan ouma <joanouma48@gmail.com>
2026-08-07 00:16:53 +0300

    Implemented smooth scrolling behavior for anchor links

--- js/main.js
+++ js/main.js
@@ -38,3 +38,13 @@ document.addEventListener('DOMContentLoaded', () => {
         anchor.addEventListener('click', function (e) {
             const targetId = this.getAttribute('href');
             if(targetId === '#') return;
+            
+            const target = document.querySelector(targetId);
+            if (target) {
+                e.preventDefault();
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
